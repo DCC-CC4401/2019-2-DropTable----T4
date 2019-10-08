81c5850aee86cc2e84f0d23836392204e56e3b71
Cristóbal Loyola <cloyola@dcc.uchile.cl>
2019-10-08 12:18:14 -0300

    Se agrega funcionalidad de cambio de constraseña.

--- tarea_4/user_registration/views.py
+++ tarea_4/user_registration/views.py
@@ -1,5 +1,8 @@
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.shortcuts import render
 from django.views.generic.edit import CreateView
 
 from .forms import CustomUserCreationForm
@@ -8,3 +11,21 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'change_password.html', {
+        'form': form
+    })
