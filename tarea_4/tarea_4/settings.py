b56d57147f752f1d359632e2c36adb7b81a0f178
Cristóbal Loyola <cloyola@dcc.uchile.cl>
2019-10-16 18:25:22 -0300

    Se agrega los templates de perfil de usuario y landing page

--- tarea_4/tarea_4/settings.py
+++ tarea_4/tarea_4/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = '/user_registration/static/'
 STATIC_URL = '/static/'
 
 # Add custom user
